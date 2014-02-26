35fc345c7e5e483e28c78bb8a6f2314283c61b9a
Jeremy Walker <jez.walker@gmail.com>
2014-02-26 16:42:45 +0000

    Fix issue with SDK secret being incorrectly set to nil.

--- lib/larva/configurator.rb
+++ lib/larva/configurator.rb
@@ -21,8 +21,10 @@ module Larva
       if meducation_sdk_config = parse_config_file('meducation-sdk.yml')
         MeducationSDK.config do |config|
           config.access_id  = meducation_sdk_config[:access_id]
-          config.secret_key = @options.fetch(:meducation_sdk_secret_key,  meducation_sdk_config[:secret_key])
-          config.endpoint   = @options.fetch(:meducation_sdk_endpoint, "http://localhost:3000/system") if @env == 'development'
+
+          # Don't use fetch for these as nil values might be deliberately passed it
+          config.secret_key = @options[:meducation_sdk_secret_key] || meducation_sdk_config[:secret_key]
+          config.endpoint   = @options[:meducation_sdk_endpoint ] || "http://localhost:3000/system" if @env == 'development'
           config.logger     = Filum.logger
         end
       end
--- lib/larva/version.rb
+++ lib/larva/version.rb
@@ -1,3 +1,3 @@
 module Larva
-  VERSION = '0.7.0'
+  VERSION = '0.7.1'
 end
