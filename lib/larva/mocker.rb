834040ed4613024c674d5c06e802808a882b406e
Jeremy Walker <jez.walker@gmail.com>
2014-03-07 16:13:44 +0000

    Use /spi, not /system in SDK mocker

--- lib/larva/mocker.rb
+++ lib/larva/mocker.rb
@@ -6,7 +6,7 @@ module Larva
 
       if const_defined?("MeducationSDK")
         MeducationSDK.config do |config|
-          config.endpoint         = "http://localhost:3000/system"
+          config.endpoint         = "http://localhost:3000/spi"
           config.recommender_host = "localhost"
           config.access_id        = "Daemon"
           config.secret_key       = "foobar"
